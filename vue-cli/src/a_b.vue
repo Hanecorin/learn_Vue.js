d451685c29cbe22466f3f7a5559fd8670b02f02e
Hanecorin <sdh1541@naver.com>
2023-08-14 14:57:17 +0900

    230814 vue cli learn

---
+++ vue-cli/src/a_b.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        header
+    </div>
+</template>
+
+<script>
+export default {
+    // Javascript - 인스턴스 옵션
+    methods: {
+        addNum: function () {
+
+        }
+    }
+
+}
+</script>
+
+<style>
+/* css */
+</style>
